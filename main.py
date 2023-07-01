aefeba12a6fef9dc8b4fb28bc083497c2bc182ba
Burak Türker <pythonu0@gmail.com>
2023-07-01 15:52:38 +0300

    Turtle is tired, but time is added

--- main.py
+++ main.py
@@ -1,25 +1,27 @@
 import turtle
 import random
+import time
 
 Game_Screen = turtle.Screen()
 Game_Screen.title("Aim Trainer")
-Game_Screen.bgcolor("#cd7014") #cd7014
+Game_Screen.bgcolor("#cd7014")  # cd7014
 
 turtle_instance = turtle.Turtle()
 turtle_instance.shape("turtle")
 turtle_instance.color("green")
 
-random_x=0.0
-random_y=0.0
+time_left = 15
+random_x = 0.0
+random_y = 0.0
 
-turtle_write_skor=turtle.Turtle()
-turtle_write_time=turtle.Turtle()
+turtle_write_skor = turtle.Turtle()
+turtle_write_time = turtle.Turtle()
 
 turtle_write_skor.penup()
 turtle_write_time.penup()
 
-turtle_write_skor.goto(0,300)
-turtle_write_time.goto(0,250)
+turtle_write_skor.goto(0, 300)
+turtle_write_time.goto(0, 250)
 
 turtle_write_skor.hideturtle()
 turtle_write_skor.isvisible()
@@ -27,9 +29,19 @@ turtle_write_skor.isvisible()
 turtle_write_time.hideturtle()
 turtle_write_time.isvisible()
 
-yazi_skor=turtle_write_skor.write("skor: ",move=True,align='center',font=('arial',12,),)
-yazi_sure=turtle_write_time.write("Süre: ",move=True,align='center',font=('arial',12,))
 
+def turtle_time():
+    global time_left
+    while time_left > 0:
+        turtle_write_time.clear()
+        turtle_write_time.write(f"Süre: {time_left}", move=False, align='center', font=('arial', 12, ''))
+        time_left -= 1
+        time.sleep(1)
+
+
+
+yazi_skor = turtle_write_skor.write("Skor: ", move=False, align='center', font=('arial', 12,''))
+turtle_time()
 
 while True:
     random_x = random.randint(-200, 200)
@@ -38,5 +50,4 @@ while True:
     turtle_instance.speed(1)
     turtle_instance.setpos(random_x, random_y)
     turtle_instance.pendown()
-
 turtle.mainloop()
