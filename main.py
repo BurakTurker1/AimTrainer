d66df523c69a64bea1957714edd893a89c6114f9
Burak Türker <pythonu0@gmail.com>
2023-07-02 17:35:52 +0300

    It's completed, but I will be editing it

--- main.py
+++ main.py
@@ -1,53 +1,122 @@
-import turtle
 import random
-import time
+import turtle
 
-Game_Screen = turtle.Screen()
-Game_Screen.title("Aim Trainer")
-Game_Screen.bgcolor("#cd7014")  # cd7014
+screen = turtle.Screen()
+game_over = False
+score = 0
+FONT = ('Arial', 30, 'normal')
 
-turtle_instance = turtle.Turtle()
-turtle_instance.shape("turtle")
-turtle_instance.color("green")
+#turtle list
+turtle_list = []
 
-time_left = 15
-random_x = 0.0
-random_y = 0.0
+#countdown turtle
+count_down_turtle = turtle.Turtle()
 
-turtle_write_skor = turtle.Turtle()
-turtle_write_time = turtle.Turtle()
+# score turtle
+score_turtle = turtle.Turtle()
 
-turtle_write_skor.penup()
-turtle_write_time.penup()
+def setup_score_turtle():
+    score_turtle.hideturtle()
+    score_turtle.color("blue")
+    score_turtle.penup()
 
-turtle_write_skor.goto(0, 300)
-turtle_write_time.goto(0, 250)
+    top_height = screen.window_height() / 2  # positive height/2 is the top of the screen
+    y = top_height - top_height / 10  # decreasing a bit so text will be visible
+    score_turtle.setposition(0, y)
+    score_turtle.write(arg='Score: 0', move=False, align='center', font=FONT)
 
-turtle_write_skor.hideturtle()
-turtle_write_skor.isvisible()
+grid_size = 10
 
-turtle_write_time.hideturtle()
-turtle_write_time.isvisible()
+def make_turtle(x, y):
+    t = turtle.Turtle()
+
+    def handle_click(x, y):
+        global score
+        score += + 1
+        score_turtle.clear()
+        score_turtle.write("Score: {}".format(score), move=False, align="center", font=FONT)
+        print(x, y)
+
+    t.onclick(handle_click)
+    t.penup()
+    t.shape("turtle")
+    t.shapesize(2, 2)
+    t.color("green")
+    t.goto(x * grid_size, y * grid_size)
+    t.pendown()
+    turtle_list.append(t)
 
 
-def turtle_time():
-    global time_left
-    while time_left > 0:
-        turtle_write_time.clear()
-        turtle_write_time.write(f"Süre: {time_left}", move=False, align='center', font=('arial', 12, ''))
-        time_left -= 1
-        time.sleep(1)
+x_coordinates = [-20, -10, 0, 10, 20]
+y_coordinates = [20, 10, 0, -10]
+
+def setup_turtles():
+    for x in x_coordinates:
+        for y in y_coordinates:
+            make_turtle(x, y)
+    '''
+    make_turtle(-20,20)
+    make_turtle(-10,20)
+    make_turtle(-0,20)
+    make_turtle(10,20)
+    make_turtle(20,20)
+    make_turtle(-20,10)
+    make_turtle(-10,10)
+    make_turtle(-0,10)
+    make_turtle(10,10)
+    make_turtle(20,10)
+    make_turtle(-20,0)
+    make_turtle(-10,0)
+    make_turtle(-0,0)
+    make_turtle(10,0)
+    make_turtle(20,0)
+    make_turtle(-20,-10)
+    make_turtle(-10,-10)
+    make_turtle(-0,-10)
+    make_turtle(10,-10)
+    make_turtle(20,-10)
+    '''
+
+def hide_turtles():
+    for t in turtle_list:
+        t.hideturtle()
+
+def show_turtles_randomly():
+    if not game_over:
+        hide_turtles()
+        random.choice(turtle_list).showturtle()
+        screen.ontimer(show_turtles_randomly, 500)
 
 
+def countdown(time):
+    global game_over
+    top_height = screen.window_height() / 2
+    y = top_height - top_height / 10
+    count_down_turtle.hideturtle()
+    count_down_turtle.penup()
+    count_down_turtle.setposition(0, y - 30)
+    count_down_turtle.clear()
 
-yazi_skor = turtle_write_skor.write("Skor: ", move=False, align='center', font=('arial', 12,''))
-turtle_time()
+    if time > 0:
+        count_down_turtle.clear()
+        count_down_turtle.write("Time: {}".format(time),move=False,align="center",font=FONT)
+        screen.ontimer(lambda: countdown(time - 1), 1000)
+    else:
+        game_over = True
+        count_down_turtle.clear()
+        hide_turtles()
+        count_down_turtle.write("Game Over!", align='center', font=FONT)
 
-while True:
-    random_x = random.randint(-200, 200)
-    random_y = random.randint(-200, 200)
-    turtle_instance.penup()
-    turtle_instance.speed(1)
-    turtle_instance.setpos(random_x, random_y)
-    turtle_instance.pendown()
+def start_game_up():
+    global game_over
+    game_over = False
+    turtle.tracer(0)
+    setup_score_turtle()
+    setup_turtles()
+    hide_turtles()
+    show_turtles_randomly()
+    turtle.tracer(1)
+    screen.ontimer(lambda: countdown(10), 10)
+
+start_game_up()
 turtle.mainloop()
